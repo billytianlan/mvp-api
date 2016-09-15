2796b83738aa8adbf2ffd38b05bd6695a111a1d5
Billy Lan <billylan22@gmail.com>
2016-09-15 13:23:18 -0700

    Add comments to vehicleModel

--- server/vehicles/vehicleModel.js
+++ server/vehicles/vehicleModel.js
@@ -1,14 +1,17 @@
 const gmApi = require('../manufacturers/gm/gmApi'); 
 
 const Vehicle = (vehicleId) => {
+  //Create vehicle instance
   let vehicle = {};
 
   //In the future we can have some logic that dispatches to the appropriote manufacturer API
+  //gmApi methods take vehicleId closure variable which is assigned during instantiation
   vehicle.getData = () => gmApi.getVehicleInfoService(vehicleId);
   vehicle.getSecurityData = () => gmApi.getSecurityStatusService(vehicleId);
   vehicle.getEnergyData = energy => gmApi.getEnergyService(vehicleId, energy);
   vehicle.actionEngine = action => gmApi.actionEngineService(vehicleId, action);
 
+  //Return instance 
   return vehicle;
 }
 
