4ee4a6f9dfc9c220d9c4dd5cd43b1b95f4932230
Billy Lan <billylan22@gmail.com>
2016-09-15 17:54:08 -0700

    Implement api test for security

--- test/gmApiSpec.js
+++ test/gmApiSpec.js
@@ -31,14 +31,72 @@ describe('GM API', () => {
       .then(response => {
         expect(response.status).to.equal('404');
       });
-    })
+    });
 
-    it('should return a reason for non 200 responses', () => {
+    it('should return a reason and status for non 200 responses', () => {
       return gmApi.getVehicleInfoService('1236')
       .then(response => {
         expect(response).to.have.all.keys('status', 'reason');
       });
-    })
+    });
+  });
+
+  describe('getSecurityStatusService', () => {
+
+    it('should return the expected data format', () => {
+      return gmApi.getSecurityStatusService('1234')
+      .then(response => {
+        expect(response).to.have.all.keys('service', 'status', 'data')
+        expect(response).to.have.deep.property('data.doors.values')
+      });
+    });
+
+    it('should return the door data as an array', () => {
+      return gmApi.getSecurityStatusService('1234')
+      .then(response => {
+        expect(response.data.doors.values).to.be.an('array');
+      });
+    });
+
+    it('should return the correct amount of doors for a sedan', () => {
+      return gmApi.getSecurityStatusService('1234')
+      .then(response => {
+        expect(response.data.doors.values).to.have.length(4);
+      });
+    });
+
+    it('should return the correct amount of doors for a coupe', () => {
+      return gmApi.getSecurityStatusService('1235')
+      .then(response => {
+        expect(response.data.doors.values).to.have.length(2);
+      });
+    });
+
+    it('should return door data in the expected format', () => {
+      return gmApi.getSecurityStatusService('1234')
+      .then(response => {
+        response.data.doors.values.forEach((door) => {
+          expect(door).to.have.all.keys('location', 'locked');
+          expect(door).to.have.deep.property('location.value')
+          expect(door).to.have.deep.property('locked.value')
+        });
+      });
+    });
+
+    it('should return an object with the status of 404 for an invalid vehicle id', () => {
+      return gmApi.getSecurityStatusService('1236')
+      .then(response => {
+        expect(response.status).to.equal('404');
+      });
+    });
+
+    it('should return a reason and status for non 200 responses', () => {
+      return gmApi.getSecurityStatusService('1236')
+      .then(response => {
+        expect(response).to.have.all.keys('status', 'reason');
+      });
+    });
+
   });
 
 });
